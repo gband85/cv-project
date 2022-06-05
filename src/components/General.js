7833c299ef512057e7529096738bb827491911dc
gband85 <garrett@gband.net>
2022-06-05 12:09:06 -0500

    create education, general, and profession sections

---
+++ src/components/General.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class General extends Component {
+    constructor() {
+        super();
+    }
+
+render() {
+    return (
+        <div>
+        <label>Name</label>
+<input className="uh"/>
+        <label>Email</label>
+<input className="uh"/>
+        <label>Phone</label>
+<input className="uh"/>
+        </div>
+    )
+}
+
+}
+
+export default General;
---
+++ src/components/Profession.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Profession extends Component {
+    constructor() {
+      super();
+    }
+
+    render() {
+        return (
+            <div>
+ <label>Company</label>
+<input className="uh"/>
+        <label>Position</label>
+<input className="uh"/>
+        <label>Tasks</label>
+<textarea className="uh"></textarea>
+<label>Start date</label>
+<input className="uh"/>
+        <label>End date</label>
+<input className="uh"/>
+ 
+            </div>
+        )
+    }
+}
+
+export default Profession;
