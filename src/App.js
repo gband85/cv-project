9e43794ec071c51911c75a2d497ee786f0557c2e
gband85 <garrett@gband.net>
2022-06-05 12:07:41 -0500

    add profession and general sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import General from './components/General';
+import Profession from './components/Profession';
+import Education from './components/Education';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<General/>
+<Profession />
+</div>
   );
+}
 }
 
 export default App;
