5741081efb12fbc42340c099620a5ce830de370f
gband85 <garrett@gband.net>
2022-06-06 11:21:50 -0500

    finish general section

--- src/components/General.js
+++ src/components/General.js
@@ -1,20 +1,52 @@
+//import { getSuggestedQuery } from "@testing-library/react";
 import React, { Component } from "react";
 
 class General extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        // this.state={
+        // //    resume: {
+        //    name: "",
+        //    email: "",
+        //    phone: "",
+        // //    },
+        // };
+      //  this.handleChange=this.handleChange.bind(this);
     }
 
+
+
 render() {
+     const { name,email,phone,handleChange }={...this.props};
+    // const {handleChange}=this.props;
     return (
-        <div>
-        <label>Name</label>
-<input className="uh"/>
-        <label>Email</label>
-<input className="uh"/>
-        <label>Phone</label>
-<input className="uh"/>
-        </div>
+
+         <form>
+        <label htmlFor="name">Name</label>
+<input
+name="name"
+    className="uh" 
+    onChange={handleChange}
+value={name}
+type="text"
+id="name"
+/>
+        <label htmlFor="email">Email</label>
+<input className="uh"
+    onChange={handleChange}
+value={email}
+type="email"
+id="email"
+/>
+        <label htmlFor="phone">Phone</label>
+<input className="uh" 
+    onChange={handleChange}
+    value={phone}
+    type="number"
+    id="phone"
+/>
+</form> 
+        
     )
 }
 
