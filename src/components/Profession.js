2487181742291e0bdb0298c63d4e8881db39e2d2
gband85 <garrett@gband.net>
2022-06-06 11:17:51 -0500

    create job section

--- src/components/Profession.js
+++ src/components/Profession.js
@@ -1,25 +1,57 @@
 import React, { Component } from "react";
 
 class Profession extends Component {
-    constructor() {
-      super();
+    constructor(props) {
+      super(props);
     }
 
     render() {
+        const {company,position,tasks,positionStart,positionEnd,
+            handleChange} = this.props;
         return (
-            <div>
- <label>Company</label>
-<input className="uh"/>
-        <label>Position</label>
-<input className="uh"/>
-        <label>Tasks</label>
-<textarea className="uh"></textarea>
-<label>Start date</label>
-<input className="uh"/>
-        <label>End date</label>
-<input className="uh"/>
+            <form>
+ <label htmlFor="company">Company</label>
+<input className="uh"
+  name="company"
+onChange={handleChange}
+value={company}
+type="text"
+id="company"
+/>
+        <label htmlFor="position">Position</label>
+<input className="uh"
+   name='position'
+   onChange={handleChange}
+   value={position}
+  type='text'
+  id="position"
+/>
+        <label htmlFor="tasks">Tasks</label>
+<textarea className="uh"
+name='tasks'
+onChange={handleChange}
+value={tasks}
+id="tasks"
+></textarea>
+<label htmlFor="positionStart">Start date</label>
+<input className="uh"
+   name='startDate'
+   onChange={handleChange}
+   value={positionStart}
+  type='date'
+  id="positionStart"
+/>
+        <label htmlFor="positionEnd">End date</label>
+<input className="uh"
+   name='endDate'
+   onChange={handleChange}
+   value={positionEnd}
+  type='date'
+  id="positionEnd"
+/>
+<button type='submit'>Save</button>
  
-            </div>
+</form>
         )
     }
 }
