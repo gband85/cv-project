4eda6a6f574a950d52a0a066b476079893b6c210
gband85 <garrett@gband.net>
2022-06-06 10:25:16 -0500

    reorganize code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import './App.css';
+import './styles/App.css';
 import React, { Component } from 'react';
 import General from './components/General';
 import Profession from './components/Profession';
 import Education from './components/Education';
 
 class App extends Component {
-  constructor() {
-    super();
-  }
+  constructor(props) {
+    super(props);
+    this.state={
+      //    resume: {
+         name: "",
+         email: "",
+         phone: "",
+      //    },
+      };
+  //   this.state={
+  //     //  resume: {
+  //       name: "",
+  //       email: "",
+  //       phone: "",
+  //       company: "",
+  //       position: "",
+  //       tasks: "",
+  //       startDate: "",
+  //       endDate: "",
+  //  data:[],
+  // }
+  this.handleChange=this.handleChange.bind(this);
+}
+  handleChange=(e)=>{
+    this.setState({
+     //   resume: {
+            [e.target.name]: e.target.value,
+       // }
+    });
+};
+// onSubmitTask = (e) => {
+//   e.preventDefault();
+//   this.setState({
+// data: this.state.data.concat(this.state.name,this.state.email)
+//   })
+// };
   render() {
+    // const {name,email,phone,company,position,tasks,startDate,endDate,data}  = this.state;
   return (
-<div>
-<General/>
-<Profession />
-</div>
+    <div>
+<General handleChange={this.handleChange}/>
+    </div>
   );
 }
 }
