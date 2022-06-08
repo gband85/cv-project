369d9946f0915ddca94a721d91da93c88e54df58
gband85 <garrett@gband.net>
2022-06-08 10:54:24 -0500

    remove constructor

--- src/components/General.js
+++ src/components/General.js
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
 
 class General extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            
- <ul>
-      
- <li>{this.props.name}</li>
- <li>{this.props.email}</li>
- <li>{this.props.phone}</li>
-
-            
+  render() {
+    return (
+      <ul>
+        <li>{this.props.name}</li>
+        <li>{this.props.email}</li>
+        <li>{this.props.phone}</li>
       </ul>
-           
-      
-)
-      }
+    );
+  }
 }
 
 export default General;
