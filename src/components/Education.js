6e1793b06e2cce18ebf7c2fef66ba9724fa79be0
gband85 <garrett@gband.net>
2022-06-09 14:21:01 -0500

    add school location and rearrange divs

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,14 +9,17 @@ class Education extends Component {
         
         return (
             <div className="section">
+            <p className="section-title">Education</p>
+<div>
             <ul>
-                <li className="section-title">{this.props.schoolDegree}</li>
-                <li className="section-place">{this.props.schoolName}</li>
-                <li>{this.props.schoolStart} to {this.props.schoolEnd}s</li>
+                <li className="position-title">{this.props.schoolDegree}</li>
+                <li className="section-place">{this.props.schoolName} - {this.props.schoolLocation}</li>
+                <li>{this.props.schoolStart} to {this.props.schoolEnd}</li>
                             </ul>
             <div>
                 <button type="button" onClick={this.props.editEducationFn}>Edit</button>
             </div>            
+            </div>
             </div>
         )
     }
