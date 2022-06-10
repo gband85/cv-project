ba52c50ebc002ccf706e6e3c0a914186398f9c3f
gband85 <garrett@gband.net>
2022-06-10 11:37:09 -0500

    use map to iterate over schools array and return properties of each element

--- src/components/Education.js
+++ src/components/Education.js
@@ -14,17 +14,23 @@ class Education extends Component {
             <p className="section-title">Education</p>
 <div className="section-body">
             <ul>
-                <li className="section-position">{this.props.schoolDegree}</li>
-                <li className="section-place">{this.props.schoolName} - {this.props.schoolLocation}</li>
-                <li className="section-dates">{this.props.schoolStart} to {this.props.schoolEnd}</li>
-                            </ul>
-            <div>
-                <button type="button" onClick={this.props.editEducationFn}><Icon path={mdiPencil}
+            {/*In schools array, for each item,iterate over properties and return p element with property*/}
+            {this.props.schools.map(function(school){
+            return   <li> {Object.values(school).map((i)=>{
+                    return <p>{i}</p>
+                })}<button type="button" ><Icon path={mdiPencil}
         size={1}
         horizontal
         vertical
         rotate={180}
-        /></button>
+        /></button></li>
+            })}
+                {/* <li className="section-position">{this.props.schoolDegree}</li>
+                <li className="section-place">{this.props.schoolName} - {this.props.schoolLocation}</li>
+                <li className="section-dates">{this.props.schoolStart} to {this.props.schoolEnd}</li> */}
+                            </ul>
+            <div>
+                
             </div>            
             </div>
             </div>
