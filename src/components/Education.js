9a88778a2d179d67814efa5a49ac267bed421527
gband85 <garrett@gband.net>
2022-06-11 12:18:04 -0500

    Moved each school li into its own component

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,32 +2,25 @@ import React, { Component } from "react";
 import '../styles/sections.css'
 import Icon from '@mdi/react';
 import { mdiPencil } from '@mdi/js'; 
+import School from './School';
 
 class Education extends Component {
-    constructor(props) {
-        super(props)
-    }
+  
     render() {
-        
+        const {editEducationFn}=this.props;
         return (
             <div className="section">
             <p className="section-title">Education</p>
 <div className="section-body">
             <ul>
+
             {/*In schools array, for each item,iterate over properties and return p element with property*/}
-            {this.props.schools.map(function(school){
-            return   <li> {Object.values(school).map((i)=>{
-                    return <p>{i}</p>
-                })}<button type="button" ><Icon path={mdiPencil}
-        size={1}
-        horizontal
-        vertical
-        rotate={180}
-        /></button></li>
+            {this.props.schools.map((school)=>{
+            return   <School {...school}/>
+                
+            
             })}
-                {/* <li className="section-position">{this.props.schoolDegree}</li>
-                <li className="section-place">{this.props.schoolName} - {this.props.schoolLocation}</li>
-                <li className="section-dates">{this.props.schoolStart} to {this.props.schoolEnd}</li> */}
+
                             </ul>
             <div>
                 
