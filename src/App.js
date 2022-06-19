ace73a33029f2cd12f6747e3bc696b71d9381e54
gband85 <garrett@gband.net>
2022-06-18 20:00:28 -0500

    set schools in state and add addSchool

--- src/App.js
+++ src/App.js
@@ -1,177 +1,75 @@
 import "./styles/App.css";
 import React, { Component } from "react";
-import EducationForm from "./components/EducationForm";
-import Profession from "./components/Profession";
-//import Education from "./components/Education";
-import General from "./components/General";
-import GeneralForm from "./components/GeneralForm";
+import SchoolForm from "./components/SchoolForm";
+
 import School from "./components/School";
 import "./styles/sections.css";
 import uniqid from "uniqid";
+import Icon from '@mdi/react';
+import { mdiPlusBoxOutline } from '@mdi/js'; 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editGeneral: false,
-      editEducation: true,
-      editProfession: true,
+      showForm:false,
       name: "Garrett Anderson",
       email: "keaton85@gmail.com",
       phone: "2566048356",
-      school: {
-      schoolDegree: "",
-      schoolName: "",
-      schoolLocation: "",
-      schoolStart: "",
-      schoolEnd: "",
-      id: uniqid(),
-    },
-    schools: [
-      {
-        schoolDegree: "Bachelor of Science in Biology2",
-        schoolName: "University of Alabama in Huntsville",
-        schoolLocation: "Huntsville, AL",
-        schoolStart: "2008-10-22",
-        schoolEnd: "2012-11-02",
-        id: uniqid(),
-      },
-      {
-        schoolDegree: "Bachelor of Science in Biology3",
-        schoolName: "University of Alabama in Huntsville",
-        schoolLocation: "Huntsville, AL",
-        schoolStart: "2008-10-22",
-        schoolEnd: "2012-11-02",
-        id: uniqid(),
-      },
-    ],
-
-      jobs: [{
-        position: "Head Widgeteer",
-        company: "Widgets, Inc.",
-        companyLocation: "Rochester, NY",        
-        tasks: [
-          "Build widgets from appropriate source materials",
-          "Fix widgets that are returned",
-          "Sell widgets on website",
-        ],
-        positionStart: "2013-02-13",
-        positionEnd: "2019-11-02",
-        id: uniqid(),
-      },
-      {
-        position: "Head Widgeteer",
-        company: "Widgets, Inc.",
-        companyLocation: "Rochester, NY",        
-        tasks: [
-          "Build widgets from appropriate source materials",
-          "Fix widgets that are returned",
-          "Sell widgets on website",
-        ],
-        positionStart: "2013-02-13",
-        positionEnd: "2019-11-02",
-        id: uniqid(),
-      }],
-      job: {
-        position: "",
-        company: "",
-        companyLocation: "",        
-        tasks: [
-          
-        ],
-        positionStart: "2013-02-13",
-        positionEnd: "2019-11-02",
-        id: uniqid(),
-      },
+    schools: this.props.degrees,
+      jobs: [],
     };
-    this.handleGeneralChange=this.handleGeneralChange.bind(this);
-    this.handleEducationChange = this.handleEducationChange.bind(this);    
-    this.editGeneralFn = this.editGeneralFn.bind(this);
-    this.editEducationFn=this.editEducationFn.bind(this);
-    this.onSubmitEducation=this.onSubmitEducation.bind(this);
-    this.onSubmitGeneral=this.onSubmitGeneral.bind(this);
+ 
   }
 
-  handleGeneralChange = (e) => {
+  addSchool=(school)=>{
+    const newSchool={...school,id:"school-"+uniqid()}
+    console.log(newSchool)
+    // this.setState({
+    //  // editEducation: false,
+    //   schools: this.state.schools.concat(school),
+    // })
+    this.setSchools([...this.state.schools,newSchool])
+  }
+  setSchools=(value)=>{
     this.setState({
-      [e.target.id]: e.target.value,
-    });
-  };
-  handleEducationChange = (e) => {
-    this.setState({
-      school: {
-        ...this.state.school,
-        [e.target.id]: e.target.value,
-        id: this.state.school.id,
-      },
-    });
-  };
-  
-  onSubmitGeneral = (e) => {
-    e.preventDefault();
-    this.setState({
-      editGeneral: false,
-    });
-  };
-
-  onSubmitEducation = (e) => {
-       e.preventDefault();
-    this.setState({
-      editEducation: false,
-      schools: this.state.schools.concat(this.state.school),
-      school: {
-        schoolDegree: "",
-        schoolName: "",
-        schoolLocation: "",
-        schoolStart: "",
-        schoolEnd: "",
-        id: uniqid(),
-      },
-    });
- 
-  };
-  
-  editGeneralFn = () => {
-    this.setState({
-      editGeneral: true,
-    });
-    // return <GeneralForm onSubmitGeneral={this.onSubmitGeneral} handleChange={this.handleChange} {...this.state}/>
-  };
-  editSchool = (id,newSchool) => {
-    this.setState({
-      editEducation: true,
-    });
-  };
-  
+     schools:value
+    })
+  }
   render() {
-
-    
+   
     return (
       <div className="container">
               <div className="section">
         <p className="section-title">Education</p>
+        <button type="button" onClick={()=>{this.setState({showForm:true})}}><Icon path={mdiPlusBoxOutline}
+    size={1}
+    horizontal
+    vertical
+    rotate={180}
+    /></button>
         <div className="section-body">
+        <SchoolForm addSchool={this.addSchool}/>
           <ul>
 
             {/*In schools array, for each item,iterate over properties and return p element with property*/}
-            {this.state.schools.map((school1) => {
+            {this.state.schools.map((school) => {
               return (
                 <School
-                  schoolDegree={school1.schoolDegree}
-                  schoolName={school1.schoolName}
-                  schoolLocation={school1.schoolLocation}
-                  schoolStart={school1.schoolStart}
-                  schoolEnd={school1.schoolEnd}
-                  
+                id={school.id}
+                school={school}
+key={school.id}
+                deleteSchool={this.deleteSchool}
+                editSchool={this.editSchool}
+
                 />
+
               );
             })}
           </ul>
           </div>
 
           </div>
-          <EducationForm onSubmitEducation={this.onSubmitEducation} handleEducationChange={this.handleEducationChange}/>
-
       </div>    );
   }
 }
