1bc3b5624919f7279f3ccdaf67ab48ba7fc10d67
gband85 <garrett@gband.net>
2022-06-21 12:42:35 -0500

    add view and edit templates

--- src/components/General.js
+++ src/components/General.js
@@ -1,36 +1,81 @@
 import React, { Component } from "react";
-import '../styles/sections.css'
-import '../styles/General.css'
-import Icon from '@mdi/react';
-import { mdiPencil } from '@mdi/js'; 
+import "../styles/sections.css";
+import "../styles/General.css";
+import Icon from "@mdi/react";
+import { mdiPencil } from "@mdi/js";
 
 class General extends Component {
-      constructor(props) {
-            super(props)
+  constructor(props) {
+    super(props);
+    this.state={
+      editMode: false,
+      newData:{
+        name:"",
+        email:"",
+        phone:"",
       }
-  render() {
-    return (
-          <div className="section">
-          <div className="section-body">
-      <ul>
-        <li className="general-title">{this.props.name}</li>
-        <li>{this.props.email}</li>
-        <li>{this.props.phone}</li>
-        </ul>
-        <div>
-      <button type="button" onClick={this.props.editGeneralFn}>     <Icon path={mdiPencil}
-        size={1}
-        horizontal
-        vertical
-        rotate={180}
-        />
-</button>
-      </div>
-      </div>
-      </div>
-
-    )
     }
+  }
+  
+  render() {
+   const editTemplate=(
+      <form onSubmit={onSubmitGeneral}>
+         <div className="input-field">
+        <label htmlFor="name" >Name</label>
+<input className="" onChange={this.handleChange} value={this.state.name} type="text" id="name"/>
+</div>
+<div className="input-field">
+        <label htmlFor="email">Email</label>
+<input className=""
+    onChange={this.handleChange}
+value={this.state.email}
+type="email"
+id="email"
+/>
+</div>
+<div className="input-field">
+        <label htmlFor="phone">Phone</label>
+<input className="" 
+    onChange={this.handleChange}
+    value={this.state.phone}
+    type="number"
+    id="phone"
+/>
+</div>
+<div className="btn-group">
+<button type='submit'>Save</button>
+</div>
+</form> 
+    )
+    const viewTemplate=(
+      <li className="section-body">
+     
+        <p className="general-title">{this.props.name}</p>
+        <p>{this.props.email}</p>
+        <p>{this.props.phone}</p>
+      
+      
+        <button type="button" onClick={this.props.editGeneralFn}>
+          <Icon
+            path={mdiPencil}
+            size={1}
+            horizontal
+            vertical
+            rotate={180}
+          />
+        </button>
+      
+    </li>
+    )
+   
+if (this.state.editMode) {
+  return editTemplate
+}
+else {
+  return viewTemplate
+}
+    
+  }
 }
 
 export default General;
