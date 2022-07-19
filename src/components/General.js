0cc091f945eea327382694a0e3c562bbac3e608a
gband85 <garrett@gband.net>
2022-07-19 18:44:47 -0500

    remove component import

--- src/components/General.js
+++ src/components/General.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import { useState } from "react";
 import Icon from "@mdi/react";
 import { mdiPencil } from "@mdi/js";
 import {validate} from "./validate";
